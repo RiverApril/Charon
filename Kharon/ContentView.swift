e19c7c5bdb1644084ee74064d8fd2fdc84c71dfe
Emily Atlee <thatemiriver@gmail.com>
2020-12-13 23:44:23 -0700

    Initial Commit

---
+++ Kharon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kharon
+//
+//  Created by Emily Atlee on 12/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kharon/KharonApp.swift
@@ -0,0 +1,17 @@
+//
+//  KharonApp.swift
+//  Kharon
+//
+//  Created by Emily Atlee on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct KharonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
